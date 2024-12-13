600846417239fa94f00f27e30698f2d017aba7a1
Jon Dedman <jonathandedman@googlemail.com>
2024-12-13 22:00:58 +0000

    update seed order

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,11 @@ puts "Destroying all discard piles"
 DiscardPile.destroy_all
 
 ActiveRecord::Base.connection.disable_referential_integrity do
-  puts "Destroying all games"
-  Game.destroy_all
   puts "Destroying all players"
   Player.destroy_all
+  puts "Destroying all games"
+  Game.destroy_all
+
 end
 
 puts "Seeding complete"
