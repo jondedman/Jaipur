357f6df218b59a8813c099b5d4c9814ee871507b
Jon Dedman <jonathandedman@googlemail.com>
2024-12-14 13:31:59 +0000

    update redis initilizer to use redis.new

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
-# f# filepath: config/initializers/redis.rb
+# filepath: /Users/jondedman/code/twenty_four/Jaipur/config/initializers/redis.rb
 require 'redis'
 
-Redis.current = Redis.new(
+$redis = Redis.new(
   url: ENV['REDIS_URL'],
   ssl: true,
   ssl_params: {
