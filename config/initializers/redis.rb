de0418b978dcad986da3b572bf38b484db1bd5d9
Jon Dedman <jonathandedman@googlemail.com>
2024-12-14 13:50:52 +0000

    Add detailed logging to Redis initialization

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,10 @@
-# filepath: /Users/jondedman/code/twenty_four/Jaipur/config/initializers/redis.rb
 require 'redis'
 
 begin
+  redis_url = ENV['REDIS_URL']
+  puts "Initializing Redis with URL: #{redis_url}"
   $redis = Redis.new(
-    url: ENV['REDIS_URL'],
+    url: redis_url,
     ssl: true,
     ssl_params: {
       verify_mode: OpenSSL::SSL::VERIFY_NONE
@@ -12,5 +13,6 @@ begin
   puts "Redis initialized successfully"
 rescue => e
   puts "Error initializing Redis: #{e.message}"
+  puts e.backtrace.join("\n")
   $redis = nil
 end
