7e73c1ed0a2dea11a80e205947502ca742635048
Jon Dedman <jonathandedman@googlemail.com>
2024-12-14 13:23:39 +0000

    update redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,10 @@
-# filepath: config/initializers/redis.rb
+# f# filepath: config/initializers/redis.rb
 require 'redis'
 
 Redis.current = Redis.new(
   url: ENV['REDIS_URL'],
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  ssl: true,
+  ssl_params: {
+    verify_mode: OpenSSL::SSL::VERIFY_NONE
+  }
 )
