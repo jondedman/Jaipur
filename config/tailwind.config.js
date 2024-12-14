f1adcf57671eefc9aa88680fe6a25a07b17026bd
Jon Dedman <jonathandedman@googlemail.com>
2024-12-14 11:40:55 +0000

    Update Tailwind CSS configuration and ensure application setup

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,33 +1,30 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	purge: {
-		content: [
-			"./public/*.html",
-			"./app/helpers/**/*.rb",
-			"./app/javascript/**/*.js",
-			"./app/views/**/*.{erb,haml,html,slim}",
-		],
-		theme: {
-			extend: {
-				fontFamily: {
-					sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-				},
-				boxShadow: {
-					obvious: "6px 4px 10px 10px rgba(0, 0, 0, 0.4)",
-				},
-				screens: {
-					xs: "375px",
-
-					portrait: { raw: "(orientation: portrait)" },
-				},
-			},
-		},
-		plugins: [
-			require("@tailwindcss/forms"),
-			require("@tailwindcss/aspect-ratio"),
-			require("@tailwindcss/typography"),
-			require("@tailwindcss/container-queries"),
-		],
-	},
+  content: [
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        obvious: '6px 4px 10px 10px rgba(0, 0, 0, 0.4)',
+      },
+      screens: {
+        xs: '375px',
+        portrait: { raw: '(orientation: portrait)' },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ],
 };
