0161fb2cc010dc870caa982f2694c666340e8437
Jon Dedman <jonathandedman@googlemail.com>
2024-12-23 17:13:31 +0000

    update redis configurration to use peer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,14 +1,18 @@
-# filepath: /Users/jondedman/code/twenty_four/Jaipur/config/initializers/redis.rb
 require 'redis'
+require 'openssl'
 
 begin
   redis_url = ENV['REDIS_URL']
   puts "Initializing Redis with URL: #{redis_url}"
+
+  # Initialize Redis with SSL enabled and certificate verification
   $redis = Redis.new(
     url: redis_url,
     ssl: true,
     ssl_params: {
-      verify_mode: OpenSSL::SSL::VERIFY_NONE
+      verify_mode: OpenSSL::SSL::VERIFY_PEER,
+      # Optionally, you can specify the path to your CA certificates here if needed
+      # ca_file: '/path/to/ca.crt'
     }
   )
   puts "Redis initialized successfully"
