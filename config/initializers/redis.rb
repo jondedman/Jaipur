54d347c12e62084f7ffb6d2a55c5895ebe6b6828
Jon Dedman <jonathandedman@googlemail.com>
2024-12-27 19:25:47 +0000

    Update Redis initializer to correctly parse Redis URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,15 +1,20 @@
 # filepath: /Users/jondedman/code/twenty_four/Jaipur/config/initializers/redis.rb
 require 'redis'
+require 'uri'
 require 'certifi'
 
 begin
   redis_url = ENV['REDIS_URL']
+  uri = URI.parse(redis_url)
   puts "Initializing Redis with URL: #{redis_url}"
   $redis = Redis.new(
-    url: redis_url,
-    ssl: true,
+    host: uri.host,
+    port: uri.port,
+    password: uri.password,
+    ssl: uri.scheme == 'rediss',
     ssl_params: {
-      verify_mode: OpenSSL::SSL::VERIFY_NONE  # Skip SSL certificate verification
+      verify_mode: OpenSSL::SSL::VERIFY_PEER,  # Verify SSL certificate
+      ca_file: Certifi.where  # Use the certifi CA bundle
     }
   )
   puts "Redis initialized successfully"
