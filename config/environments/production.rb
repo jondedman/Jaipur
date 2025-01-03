53d80c29decc622ddf2ac43bf286cc8d36d4f4ac
Jon Dedman <jonathandedman@googlemail.com>
2025-01-03 22:01:37 +0000

    increase detail of logs in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #  The following configuration is for the Heroku Redis add-on
+  config.log_level = :debug
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
